b2bbcaf8d350cfb66f23ae2ae5997ab40c9ab107
gli32 <gli32@hawk.iit.edu>
2024-05-02 17:22:56 -0400

    create slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,55 @@
-import {createStore } from 'redux'
+import {createSlice, configureStore} from '@reduxjs/toolkit'
 
-const initialState = {counter: 0, showCounter: true};
+export const INCREMENT = 'increment'
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
+const initialCounterState = {counter: 0, showCounter: true};
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state,action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    }
+});
+
+const initialAuthState = {
+    isAuthenticated: false
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
         }
     }
+})
 
-    if (action.type === 'increase' ) {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
+
+const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
     }
 
-    if (action.type === 'decrement' ) {
-        return {
-            counter: state.counter -1,
-            showCounter: state.showCounter
-        }
-    }
-    
-    if(action.type === 'toggle' ) {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
+    //reducer : counterSlice.reducer
+});
 
-    return state;
-};
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions
+export const authActions = authSlice.actions
 export default store;
