41a7d8a0427244be4e1051abc5e733c8772edc28
gli32 <gli32@hawk.iit.edu>
2024-05-03 15:05:13 -0400

    split code

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,7 @@
 import {createSlice, configureStore} from '@reduxjs/toolkit'
+import counterSlice from './counter'
 
 export const INCREMENT = 'increment'
-
-const initialCounterState = {counter: 0, showCounter: true};
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state,action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        },
-    }
-});
 
 const initialAuthState = {
     isAuthenticated: false
